20b940ad212c0ba7dab96954de53a5812a99fb27
Sheepsody <44940387+Sheepsody@users.noreply.github.com>
2019-12-02 16:35:11 +0100

    Jit Actor Critic

--- src/networks/ActorCritic.py
+++ src/networks/ActorCritic.py
@@ -7,23 +7,33 @@ import numpy as np
 # Import predefined networks
 from src.networks.utils import *
 
+from typing import Dict
+
+try:
+    from typing_extensions import Final
+except:
+    from torch.jit import Final
+
+
+# FIXME : hints for the functions
 
 # Why using Torchscript ?
 # nn.Module inheritance
 # More efficient execution
+from enum import Enum
 
 
+class BodyType(Enum):
+    SHALLOW = 1
+    DEEP = 2
 
-class ActorCritic(torch.jit.ScriptModule):
-    """
-    Base class for the Model, with the forward and backward passes
-    => Loss methods is abstract and needs to be updated to match a specific algorithm
 
-    @torch.jit.script_method is used to declare new methods for jit (default is only forward)
-    """
-    __constants__ = ["input_size", "flatten_dim", "num_action", "hidden_size"]
+class ActorCritic(nn.Module):
+    flatten_dim: Final[int]
+    hidden_size: Final[int]
+    n_outputs: Final[int]
 
-    def __init__(self, h, w, c, n_outputs):
+    def __init__(self, h, w, c, n_outputs, body=BodyType.SHALLOW):
         """You can have several types of body as long as they implement the size function"""
         super(ActorCritic, self).__init__()
 
@@ -33,13 +43,112 @@ class ActorCritic(torch.jit.ScriptModule):
         self.hidden_size = 256
 
         # Sequential baseblocks
-        self.convs = DeepConv(c)
-        self.flatten_dim = self.conv.output_size(h, w)*32
+        if body == BodyType.SHALLOW:
+            self.convs = ShallowConv(c)
+        elif body == BodyType.DEEP:
+            self.convs = DeepConv(c)
+        else :
+            raise AttributeError("The body type is not valid")
+
+        self.flatten_dim = self.convs.output_size(h, w)*32
 
         # Fully connected layers
         self.flatten = Flatten()
         self.fc = nn.Linear(in_features=self.flatten_dim,
-                            out_features=256)
+                            out_features=self.hidden_size)
+
+        self.body = nn.Sequential(
+            self.convs,
+            Flatten(),
+            nn.ReLU(),
+            self.fc,
+            nn.ReLU()
+        )
+
+        # Fully conected layers for value and policy
+        self.value = nn.Linear(in_features=self.hidden_size,
+                               out_features=1)
+
+        self.logits = nn.Linear(in_features=self.hidden_size,
+                               out_features=self.n_outputs)
+
+        # Using those distributions doesn't affect the gradient calculus
+        # see https://arxiv.org/abs/1506.05254 for more infos
+        self.dist = Categorical
+
+    @torch.jit.export
+    def act(self, obs, lstm_hxs):
+        """Performs an one-step prediction with detached gradients"""
+        # x : (batch, input_size)
+        x = self.body.forward(obs)
+
+        logits = self.logits(x)
+
+        dist = self.dist(logits=logits)
+
+        action = dist.sample()
+        log_prob = dist.log_prob(action)
+
+        return action.detach(), log_prob.detach(), lstm_hxs.detach()
+
+    def forward(self, obs, behaviour_actions):
+        """
+        x : (seq, batch, c, h, w)
+        behaviour_actions : (seq, batch, num_actions)
+        """
+        # Check the dimentions
+        assert obs.dim() == 5
+        seq, batch, c, h, w = obs.size()
+
+        # 1. EFFICIENT COMPUTATION ON CNNs (time is folded with batch size)
+
+        obs = obs.view(seq * batch, c, h, w)
+        x = self.body.forward(obs) # x : (seq * batch, flatten_dim)
+        
+        behaviour_actions = x.view(seq * batch, self.n_outputs)
+
+        target_value = self.value(x)
+        logits = self.logits(x)
+        dist = self.dist(logits=logits)
+        target_log_probs = dist.log_prob(behaviour_actions) 
+        target_entropy = dist.entropy()
+
+        target_log_probs = target_log_probs.view(seq, batch, 1)
+        target_entropy = target_entropy.view(seq, batch, 1)
+        target_value = target_value.view(seq, batch, 1)
+
+        return target_log_probs, target_entropy, target_value
+
+
+
+class ActorCriticLSTM(nn.Module):
+    flatten_dim: Final[int]
+    hidden_size: Final[int]
+    n_outputs: Final[int]
+
+    def __init__(self, h, w, c, n_outputs, body=BodyType.SHALLOW):
+        """You can have several types of body as long as they implement the size function"""
+        super(ActorCriticLSTM, self).__init__()
+
+        # Keeping some infos
+        self.n_outputs = n_outputs
+        self.input_size = (c, h, w)
+        self.hidden_size = 256
+
+        # Sequential baseblocks
+        if body == BodyType.SHALLOW:
+            self.convs = ShallowConv(c)
+        elif body == BodyType.DEEP:
+            self.convs = DeepConv(c)
+        else :
+            raise AttributeError("The body type is not valid")
+
+        self.flatten_dim = self.convs.output_size(h, w)*32
+
+        # Fully connected layers
+        self.flatten = Flatten()
+        self.fc = nn.Linear(in_features=self.flatten_dim,
+                            out_features=self.hidden_size)
 
         self.body = nn.Sequential(
             self.convs,
@@ -50,7 +159,7 @@ class ActorCritic(torch.jit.ScriptModule):
         )
 
         # LSTM (Memory Layer)
-        self.lstm = nn.LSTM(input_size=256,
+        self.lstm = nn.LSTM(input_size=self.hidden_size,
                             hidden_size=self.hidden_size,
                             num_layers=1)
         
@@ -61,14 +170,14 @@ class ActorCritic(torch.jit.ScriptModule):
         self.value = nn.Linear(in_features=self.hidden_size,
                                out_features=1)
 
-        self.logits = nn.Linear(in_features=256,
+        self.logits = nn.Linear(in_features=self.hidden_size,
                                out_features=self.n_outputs)
 
         # Using those distributions doesn't affect the gradient calculus
         # see https://arxiv.org/abs/1506.05254 for more infos
         self.dist = Categorical
 
-    @torch.jit.script_method
+    @torch.jit.export
     def act(self, obs, lstm_hxs):
         """Performs an one-step prediction with detached gradients"""
         # x : (batch, input_size)
@@ -88,7 +197,6 @@ class ActorCritic(torch.jit.ScriptModule):
 
         return action.detach(), log_prob.detach(), lstm_hxs.detach()
 
-    @torch.jit.script_method
     def forward(self, obs, lstm_hxs, reset_mask, behaviour_actions):
         """
         x : (seq, batch, c, h, w)
@@ -100,8 +208,6 @@ class ActorCritic(torch.jit.ScriptModule):
         assert obs.dim() == 5
         seq, batch, c, h, w = obs.size()
 
-        assert lstm_hxs[0].dim() == 3
-        assert reset_mask.dim() == 3
 
         # 1. EFFICIENT COMPUTATION ON CNNs (time is folded with batch size)
 
@@ -125,7 +231,6 @@ class ActorCritic(torch.jit.ScriptModule):
 
         x = torch.cat(tensors=x_lstm, dim=0) # (seq_len, batch, input)
         
-        # 3. EFFICIENT COMPUTATION OF LAST LAYERS (time is folded with batch size)
 
         x = x.view(seq * batch, self.flatten_dim)
         behaviour_actions = x.view(seq * batch, self.n_outputs)
